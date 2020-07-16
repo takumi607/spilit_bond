44ec76591a8fc10cb86b055082077b44ac5ba723
TakumiMogi <takumi607@gmail.com>
2020-07-16 09:35:19 +0800

    add croudfund part

---
+++ project/Project/CroudFundNextViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CroudFundNextViewController.swift
+//  Project
+//
+//  Created by 茂木匠 on 2020/7/15.
+//  Copyright © 2020 茂木匠. All rights reserved.
+//
+
+import UIKit
+
+class CroudFundNextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
